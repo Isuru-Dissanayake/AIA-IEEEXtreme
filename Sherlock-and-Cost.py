17d63cee1bd199f171aca33bc3d04c5a3de38c1f
isuru <isurumailme@gmail.com>
2019-08-17 15:07:42 -0700

    Isuru

--- Sherlock-and-Cost.py
+++ Sherlock-and-Cost.py
@@ -1,16 +1,33 @@
-def getCost(b):
-    cost = []
-    for x in range(len(b)):
-        cost.append([0,0])
-    for i in range(1,len(b)):
-        cost[i][0] = cost[i-1][1] + abs(b[i-1] - 1)
-        u = cost[i-1][0] + abs(b[i] - 1)
-        v = cost[i-1][1] + abs(b[i] - b[i-1])
-        cost[i][1] = max(u,v)
-    print (max(cost[len(b) - 1][0],cost[len(b)-1][1]))
+#!/bin/python3
 
-t = int(input(""))
-for q in range(t):
-    n = int(input(""))
-    array = list(map(int,(input("")).split()))
-    getCost(array)
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the cost function below.
+def cost(B):
+    dp=[[0,0]]
+    for i in range(1,len(B)):
+        k=[abs(B[i-1]-1)+dp[i-1][1],max(abs(B[i]-1)+dp[i-1][0],abs(B[i]-B[i-1])+dp[i-1][1])]
+        dp.append(k)
+
+    return(max(dp[len(B)-1]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        B = list(map(int, input().rstrip().split()))
+
+        result = cost(B)
+
+        fptr.write(str(result) + '\n')
+        #print(str(result) + '\n')
+
+    fptr.close()
