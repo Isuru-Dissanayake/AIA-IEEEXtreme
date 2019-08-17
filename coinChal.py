80aea0d9e364f16cc3d510ac3f529df893f7bc0f
isuru <isurumailme@gmail.com>
2019-08-17 12:46:21 -0700

    isuru

--- coinChal.py
+++ coinChal.py
@@ -1,10 +1,7 @@
 #https://www.hackerrank.com/challenges/coin-change/problem?isFullScreen=false
 
-n = 4
-c = [1,2,3]
-
 def getWays(n,c):
-    array = [0]*len(c)
+    array = [0]*(n+1)
     array[0] = 1
     for i in range(len(c)):
         for j in range(len(array)):
@@ -14,6 +11,12 @@ def getWays(n,c):
             else:
                 array[j] = array[j] + array[ind]
         print (array)
-    return(array[len(c) - 1])
+    return(array[n])
+
+n = int(input(""))
+c = list(map(int,(input("")).split()))
+
+#n = 4
+#c = [1,2,3]
 
 print(getWays(n,c))
