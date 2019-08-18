8122d606ec5f30ca0d4165818f05268733788d80
isuru <isurumailme@gmail.com>
2019-08-17 18:50:08 -0700

    Isuru - not working

--- The-Maximum-Subarray.py
+++ The-Maximum-Subarray.py
@@ -2,32 +2,30 @@ def maxSubarray(array):
     subarraySum = 0
     subsquenceSum = 0
     subarraySumMax = 0
-    count = 0
-
     arrayNew = array[::-1]
+    count = 0
     for i in range(len(array)):
         if (arrayNew[i] > 0):
             arrayNew = arrayNew[i:]
+            count = 1
             break
-    if (i == len(array)):
+    if (count == 0):
         return (max(array),max(array))
     else:
         arrayNew = arrayNew[::-1]
-
-        for i in range(len(arrayNew)):
-            if (arrayNew[i] >= 0):
-                subarraySum += arrayNew[i]
+        i = 0
+        while (i < len(arrayNew)):
+            if (arrayNew[i] > 0):
+                subarraySum = subarraySum + arrayNew[i]
                 subsquenceSum += arrayNew[i]
-            elif (subarraySum > arrayNew[i]):
-                subarraySum += arrayNew[i]
-                count +=1
-            elif (subarraySum < arrayNew[i]):
-                subarraySumMax = max(subarraySum,subarraySumMax)
-                subarraySum = 0
-                count +=1
-        if count == len(arrayNew):
-            subarraySumMax = subsquenceSum = max(array)
-        
+                i+=1
+            elif ((subarraySum >= arrayNew[i]) and (arrayNew[i+1] >= arrayNew[i])):
+                subarraySum = subarraySum + arrayNew[i] + arrayNew[i+1]
+                subsquenceSum += arrayNew[i+1]
+                i+=2
+            elif (subarraySum <= arrayNew[i]):
+                subarraySumMax = max(subarraySumMax,subarraySum)
+        subarraySumMax = max(subarraySum,subarraySumMax)
         return (subarraySumMax, subsquenceSum)
 
 array = [2, -1, 2, 3, 4, -5]
