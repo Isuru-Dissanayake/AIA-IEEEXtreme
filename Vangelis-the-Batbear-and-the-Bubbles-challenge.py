3032f4ccb5093aed53dba9a8b6125c99c665cf5b
Isuru Dissanayake <isurumailme@gmail.com>
2019-09-23 17:07:28 +0530

    Update Vangelis-the-Batbear-and-the-Bubbles-challenge.py

--- Vangelis-the-Batbear-and-the-Bubbles-challenge.py
+++ Vangelis-the-Batbear-and-the-Bubbles-challenge.py
@@ -1,2 +1,14 @@
 #https://csacademy.com/ieeextreme-practice/task/979a09a0cd8c4e98dd0a690f39a55bd2/
 
+testCases = int(input(""))
+
+for cases in range(testCases):
+    nm = list(map(int,(input("")).split()))
+    e = list(map(int,(input("")).split()))
+    edges = [[] for i in range(nm[0])]
+    for i in range(len(e)):
+        if i%2 == 0:
+            edges[e[i]].append(e[i+1])
+        elif i%2 == 1:
+            edges[e[i]].append(e[i-1])
+print (edges)
