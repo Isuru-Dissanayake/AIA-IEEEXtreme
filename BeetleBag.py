2557051aeffa1040c3e2605ec46a1444411f6f3c
Isuru Dissanayake <isurumailme@gmail.com>
2019-09-26 15:28:30 +0530

    Update BeetleBag.py

--- BeetleBag.py
+++ BeetleBag.py
@@ -1,4 +1,18 @@
 #https://csacademy.com/ieeextreme-practice/task/ed8629419f140a5a2c923b049aba1224/
 
+def maxPower(w, g, w_num):
+    ans = 0
+    
+
 t = int(input(""))
-wg = list(map(int,input("").split()))
+for testCases in range(t):
+    wg = list(map(int,input("").split()))
+    w_num = wg[0]
+    g_num = wg[1]
+    w = []
+    g = []
+    for i in range(g_num):
+        wg = list(map(int,input("").split()))
+        w.append(wg[0])
+        g.append(wg[1])
+    
