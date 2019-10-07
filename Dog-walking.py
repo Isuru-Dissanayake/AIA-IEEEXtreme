33108e985b4dfae194165771cd430fb193b9110b
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-10-07 14:58:51 +0530

    Update Dog-walking.py

--- Dog-walking.py
+++ Dog-walking.py
@@ -1,53 +1,10 @@
-# a simple parser for python. use get_number() and get_word() to read
-def parser():
-    while 1:
-        data = list(input().split(' '))
-        for number in data:
-            if len(number) > 0:
-                yield(number)   
-
-input_parser = parser()
-
-def get_word():
-    global input_parser
-    return next(input_parser)
-
-def get_number():
-    data = get_word()
-    try:
-        return int(data)
-    except ValueError:
-        return float(data)
-
-# numpy and scipy are available for use
-import numpy as np
-import scipy
-
-a = get_number()
-for i in range(a):
-    ndogs= get_number()
-    nwalkers= get_number()
-    lis=[]
-    for j in range(ndogs):
-        lis.append(get_number())
-        
-    lis.sort()
-    cost= lis[-1]-lis[0]
+t = int(input(""))
+for testCases in range(t):
+    nk = list(map(int,input("").split()))
+    n = nk[0]
+    k = nk[1]
+    dogs = []
+    for i in range(n):
+        dogs.append(int(input("")))
+    dogs.sort()
     
-    liss=[]
-    current= lis[0]
-    for j in range(1,ndogs):
-        if lis[j]==current:
-            liss.append([0,j-1])
-            current=lis[j]
-            pass
-        else:
-            liss.append([lis[j]-current,j-1])
-            current=lis[j]
-            
-    liss.sort()
-    liss=liss[nwalkers*-1+1:]
-    sum=0
-    for j in range(len(liss)):
-        sum+= liss[j][0]
-    print(cost-sum)
